285b1793e5984fa221aba8cdfcadd5326e958f17
Alexanderforsbergg <alexanderforsbergg@gmail.com>
2022-01-09 14:55:09 +0100

    initial commit

---
+++ CV_Webbutveckling_Alexander/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using CV_Webbutveckling_Alexander.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace CV_Webbutveckling_Alexander.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Experience()
+        {
+            return View();
+        }
+
+        public IActionResult AboutMe()
+        {
+            return View();
+        }
+
+        public IActionResult Classes()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
